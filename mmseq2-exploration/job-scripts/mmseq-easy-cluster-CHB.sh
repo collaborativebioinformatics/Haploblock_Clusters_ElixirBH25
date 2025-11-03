1951427ed8cec1bbc00622bb673e5e5208277860
hetvi j <47458458+cosmicbhejafry@users.noreply.github.com>
2025-11-03 15:24:16 +0000

    scripts- mmseq2 on TNFa chr6 for 3 populations

---
+++ mmseq2-exploration/job-scripts/mmseq-easy-cluster-CHB.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#PBS -N mmseq_easyClust_CHB
+#PBS -l walltime=04:00:00
+#PBS -l select=1:ncpus=8:mem=16gb
+
+set -e
+set -o pipefail
+
+eval "$(~/anaconda3/bin/conda shell.bash hook)"
+source activate biohack2025_haplo
+
+echo "start at $(date)"
+
+export BASE_DIR=$HOME/BioHack2025/4-haploblocks/mmseq2-exploration/
+
+export INPUT_FASTA=$BASE_DIR/data/TNF/data/CHB/haploblock_phased_seq_TNFa/haploblock_phased_seq_merged/chr6_region_31480875-31598421.fa
+export RESULTS=TNF_CHB_chr6_region_31480875-31598421
+export TEMP_FOLDER=$EPHEMERAL/BioHack2025_tempfiles
+export MIN_SEQ_ID=0.999
+export COV_FRACTION=0.8
+export COV_MODE=0
+
+echo "min_seq_id ${MIN_SEQ_ID}"
+echo "C ${COV_FRACTION}"
+echo "cov-mode ${COV_MODE}"
+
+mkdir -p $BASE_DIR/results/$RESULTS
+cd $BASE_DIR/results/$RESULTS
+
+mmseqs easy-cluster $INPUT_FASTA $RESULTS $TEMP_FOLDER \
+            --min-seq-id $MIN_SEQ_ID -c $COV_FRACTION --cov-mode $COV_MODE
+
+echo "end at $(date)"
+echo "DONE!"
---
+++ mmseq2-exploration/readme.md
@@ -0,0 +1,4 @@
+Scripts running on PBS
+mmseqs2 installed using conda
+
+todo - put the conda yml here
