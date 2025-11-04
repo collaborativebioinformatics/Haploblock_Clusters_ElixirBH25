ffb19705f1e4d40fba936b68ffa5bf8bd86bc62f
hetvi j <47458458+cosmicbhejafry@users.noreply.github.com>
2025-11-04 11:12:42 +0000

    cluster rerun with new param &  num cluster parsing script

--- mmseq2-exploration/job-scripts/mmseq-easy-cluster-CHB.sh
+++ mmseq2-exploration/job-scripts/mmseq-easy-cluster-CHB.sh
@@ -12,13 +12,16 @@ source activate biohack2025_haplo
 echo "start at $(date)"
 
 export BASE_DIR=$HOME/BioHack2025/4-haploblocks/Haploblock_Clusters_ElixirBH25/mmseq2-exploration
+export POP_ID=CHB
 
-export INPUT_FASTA=$BASE_DIR/data/TNF/data/CHB/haploblock_phased_seq_TNFa/haploblock_phased_seq_merged/chr6_region_31480875-31598421.fa
-export RESULTS=TNF_CHB_chr6_region_31480875-31598421
+export INPUT_FASTA=$BASE_DIR/data/TNF/data/${POP_ID}/haploblock_phased_seq_TNFa/haploblock_phased_seq_merged/chr6_region_31480875-31598421.fa
+export RESULTS=TNF_${POP_ID}_chr6_region_31480875-31598421
+
 export TEMP_FOLDER=$EPHEMERAL/BioHack2025_tempfiles/$RESULTS
-export MIN_SEQ_ID=0.99
-export COV_FRACTION=0.99
+export MIN_SEQ_ID=0.9936 #derived from jedrzej script
+export COV_FRACTION=0.9942 # 1 - 682/HAPLO_LEN - 0.994198016096 rounded off to 0.9942
 export COV_MODE=0
+export PARAM_SET=2
 
 echo "min_seq_id ${MIN_SEQ_ID}"
 echo "C ${COV_FRACTION}"
@@ -26,8 +29,8 @@ echo "cov-mode ${COV_MODE}"
 
 mkdir -p $TEMP_FOLDER
 
-mkdir -p $BASE_DIR/results/params2/$RESULTS
-cd $BASE_DIR/results/params2/$RESULTS
+mkdir -p $BASE_DIR/results/params${PARAM_SET}/$RESULTS
+cd $BASE_DIR/results/params${PARAM_SET}/$RESULTS
 
 mmseqs easy-cluster $INPUT_FASTA $RESULTS $TEMP_FOLDER \
             --min-seq-id $MIN_SEQ_ID -c $COV_FRACTION --cov-mode $COV_MODE
--- mmseq2-exploration/job-scripts/parseCluster-mmseq-easy-cluster.sh
+++ mmseq2-exploration/job-scripts/parseCluster-mmseq-easy-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#PBS -N mmseq_numCluster
+#PBS -N mmseq_clusterParams
 #PBS -l walltime=01:00:00
 #PBS -l select=1:ncpus=8:mem=16gb
 
@@ -12,13 +12,18 @@ source activate biohack2025_haplo
 echo "start at $(date)"
 
 export BASE_DIR=$HOME/BioHack2025/4-haploblocks/Haploblock_Clusters_ElixirBH25/mmseq2-exploration
+export PARAM_SET=0
 
-export INPUT_FASTA=$BASE_DIR/data/TNF/data/PUR/haploblock_phased_seq_TNFa/haploblock_phased_seq_merged/chr6_region_31480875-31598421.fa
-export RESULTS=TNF_PUR_chr6_region_31480875-31598421
-export TEMP_FOLDER=$EPHEMERAL/BioHack2025_tempfiles
-export MIN_SEQ_ID=0.99
-export COV_FRACTION=0.99
-export COV_MODE=0
+export RESULTS_FOLDER=$BASE_DIR/results/params${PARAM_SET}/
+
+read p1 p2 p3 < <(tail -n +2 $RESULTS_FOLDER/params${PARAM_SET}.tsv)
+
+echo -e "MIN_SEQ_ID\tCOV_FRACTION\tCOV_MODE\tSUBFOLDER_NAME\tNUM_CLUSTER" > $RESULTS_FOLDER/params${PARAM_SET}_cluster.tsv
+
+for d in $RESULTS_FOLDER/TNF_*; do
+    n=$(grep -c "^>" $d/*_rep_seq.fasta)
+    echo -e "${p1}\t${p2}\t${p3}\t${d}\t${n}" >> $RESULTS_FOLDER/params${PARAM_SET}_cluster.tsv
+done
 
 echo "end at $(date)"
 echo "DONE!"
